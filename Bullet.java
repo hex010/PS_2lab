1c192661e9a4577141cd716776430d9beb664159
Tomasmorkunas <114162974+Tomasmorkunas@users.noreply.github.com>
2023-10-16 01:07:08 +0300

    Update Bullet.java
    Ištaisiau klaidas

--- Bullet.java
+++ Bullet.java
@@ -3,26 +3,24 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.File;
 
 public class Bullet {
     private int positionX;
     private int positionY;
     private Direction direction;
     private boolean active;
-    private int movingSpeed = 7;
+    private int movingSpeed;
     BufferedImage bulletImage;
     GamePanel gamePanel;
     Rectangle rectangle;
-    public boolean shotByPlayer;
+    private boolean shotByPlayer;
     int mode;
 
     public Bullet(GamePanel gamePanel, int mode) {
         this.gamePanel = gamePanel;
         this.mode = mode;
+        this.movingSpeed = 7;
         setImage();
-
-
         setRectangleValues();
     }
 
@@ -36,9 +34,10 @@ public class Bullet {
 
     private void setImage() {
         try {
-            bulletImage = ImageIO.read(new FileInputStream("resources/bullet.png"));
+            String resource = "resources/bullet.png";
+            bulletImage = ImageIO.read(new FileInputStream(resource));
         } catch (IOException e) {
-
+            System.out.println("Įvyko klaida.");
         }
     }
 
@@ -50,59 +49,78 @@ public class Bullet {
         this.shotByPlayer = shotByPlayer;
     }
 
-    public void travelMode() {
-        if (mode == 4) {
-        } else {
-
-        }
-    }
-
     public void update(){
 
         if(shotByPlayer) {
+            if (shotByPlayerHitTheEnemy()) return;
+        }else{
+            if (shotByEnemyHitThePlayer()) return;
+        }
+        
+        switch (direction) {
+            case UP: {
+                moveUp();
+                break;
+            }
+            case DOWN: {
+                moveDown();
+                break;
+            }
+            case LEFT: {
+                moveLeft();
+                break;
+            }
+            case RIGHT: {
+                moveRight();
+                break;
+            }
+        }
+    }
+
+    private void moveUp() {
+        if(!gamePanel.getCollision().checkBulletCollisionWithTile(this))
+            positionY -= movingSpeed;
+        else active = false;
+    }
+
+    private void moveDown() {
+        if(!gamePanel.getCollision().checkBulletCollisionWithTile(this))
+            positionX -= movingSpeed;
+        else active = false;
+    }
+
+    private void moveLeft() {
+        if(!gamePanel.getCollision().checkBulletCollisionWithTile(this))
+            positionY -= movingSpeed;
+        else active = false;
+    }
+
+    private void moveRight() {
+        if(!gamePanel.getCollision().checkBulletCollisionWithTile(this))
+            positionX += movingSpeed;
+        else active = false;
+    }
+
+    private boolean shotByPlayerHitTheEnemy() {
             int enemyIndex = gamePanel.getCollision().checkBulletWithEnemyCollision(this, gamePanel.enemies);
             if (enemyIndex != -1) {
                 gamePanel.setScore(gamePanel.getScore() + 1);
                 gamePanel.enemies.remove(enemyIndex);
                 gamePanel.addNewEnemy();
                 active = false;
-                return;
+                return true;
             }
-        }else{
+            return false
+        }
+                
+    private boolean shotByEnemyHitThePlayer() {
             if (gamePanel.getCollision().checkBulletWithPlayerCollision(this, gamePanel.getPlayer())) {
                 gamePanel.setGameOver(true);
-                return;
+                return true;
             }
-        }
-
-        switch (direction){
-            case UP: {
-                if(!gamePanel.getCollision().checkBulletCollisionWithTile(this))
-                    positionY -= movingSpeed;
-                else active = false;
-                break;
-            }
-            case DOWN: {
-                if(!gamePanel.getCollision().checkBulletCollisionWithTile(this))
-                    positionY += movingSpeed;
-                else active = false;
-                break;
-            }
-            case LEFT: {
-                if(!gamePanel.getCollision().checkBulletCollisionWithTile(this))
-                    positionX -= movingSpeed;
-                else active = false;
-                break;
-            }
-            case RIGHT: {
-                if(!gamePanel.getCollision().checkBulletCollisionWithTile(this))
-                    positionX += movingSpeed;
-                else active = false;
-                break;
-            }
-        }
+            return false;
     }
-
+    
     public void paint(Graphics g){
         g.drawImage(bulletImage, positionX, positionY, gamePanel.getTileSize(), gamePanel.getTileSize(), null);
     }
@@ -122,7 +140,6 @@ public class Bullet {
     public boolean isActive() {
         return active;
     }
-
 
     public int getMovingSpeed() {
         return movingSpeed;
